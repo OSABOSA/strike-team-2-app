86681a6871b55a8d2e1004127698536005189e12
Nstandardowy <kuba@gonczarek.com>
2025-03-31 14:29:09 +0200

    fix to download_dataset

--- src/data/dataset_downloader.py
+++ src/data/dataset_downloader.py
@@ -13,22 +13,18 @@ def download_dataset() -> None:
     kaggle_dataset_cache_folder: Path = Path(kagglehub.config.get_cache_folder()+"/datasets/ankkur13")
     
     if kaggle_dataset_cache_folder.exists(): 
-        assert not os.system(f"rm -rf {kaggle_dataset_cache_folder}")
+        shutil.rmtree(kaggle_dataset_cache_folder)
     
     dataset_path: str = kagglehub.dataset_download(handle="ankkur13/edmundsconsumer-car-ratings-and-reviews")
 
-
     if os.name == "nt":
-            for file in Path(dataset_path).iterdir():
-                shutil.move(str(file), str(RAW_DATA_FOLDER))
-            shutil.rmtree(kaggle_dataset_cache_folder, ignore_errors=True)
+        for file in Path(dataset_path).iterdir():
+            shutil.move(str(file), str(RAW_DATA_FOLDER))
+        shutil.rmtree(kaggle_dataset_cache_folder, ignore_errors=True)
     else:  # Linux/macOS
         assert not os.system(f"mv -v {dataset_path}/* {RAW_DATA_FOLDER}")
         assert not os.system(f"rm -rf {kaggle_dataset_cache_folder}")
 
-    
-    assert not os.system(f"mv -v {dataset_path}/* {RAW_DATA_FOLDER}")
-    assert not os.system(f"rm -rf {kaggle_dataset_cache_folder}")
 
 if __name__ == "__main__": 
     download_dataset()
